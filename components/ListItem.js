3cba4eee261754980a9c584cbb169ff0cfed0459
Ekaterina <ash2789@mail.ru>
2021-10-07 22:09:58 +0300

    added folders

---
+++ components/ListItem.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity, } from 'react-native';
+
+export default function ListItem({ el, deleteHandler }) {
+
+  return (
+		<TouchableOpacity onPress={() => deleteHandler(el.key)}>
+			<Text style={styles.text}>{el.text}</Text>
+		</TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  text: {
+		padding: 20,
+		textAlign: 'center',
+		borderRadius: 5,
+		backgroundColor: '#c98bcd',
+		borderWidth: 1,
+		borderStyle: 'solid',
+		borderColor: 'black',
+		marginTop: 20,
+		width: '60%',
+		marginLeft: '20%',
+	}
+});
