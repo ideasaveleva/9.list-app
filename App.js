f07a1de5d271c12c727275941298c46add328726
Ekaterina Saveleva <ash2789@mail.ru>
2021-10-08 16:31:24 +0300

    added styles and fonts

--- App.js
+++ App.js
@@ -1,44 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, View, FlatList, Text } from 'react-native';
-import Header from './components/Header';
-import ListItem from './components/ListItem';
-import Form from './components/Form';
+import { StyleSheet, View, } from 'react-native';
+import { gStyle } from './styles/style';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
+
+const fonts = () => Font.loadAsync({
+	'mt-bold': require('./assets/fonts/Montserrat-Bold.ttf'),
+	'mt-light': require('./assets/fonts/Montserrat-Light.tt'),
+});
+
 
 export default function App() {
-		const [listOfItems, setListOfItems] = useState([
-		{text: 'Стать успешным программистом', key: '1'},
-		{text: 'Забрать свой участок', key: '2'},
-		{text: 'Построить дом', key: '3'},
-		{text: 'Быть счастливым', key: '4'},
-	])
+	const [font, setFont] = useState(false);
 
-	const addHandler = (text) => {
-		setListOfItems((list) => {
-			return [
-				{text: text, key: Math.random().toString(36).substring(7)},
-				...list
-			]
-		})
-	}
-
-	const deleteHandler = (key) => {
-		setListOfItems((list) => {
-			return list.filter(listOfItems => listOfItems.key != key )
-		})
-	}
-
-  return (
-    <View style={styles.mainBlock}>
-      <Header/>
-			<Form addHandler={addHandler}/>
-			<View>
-				<FlatList data={listOfItems} renderItem={({ item }) => (
-					<ListItem el={item} deleteHandler={deleteHandler}/>
-				)}/>
+	if(font){
+		return (
+			<View style={gStyle.main}>
+			
 			</View>
-    </View>
-  );
+		);
+	} else {
+		return (
+			<AppLoading startAsync={fonts} onFinish={()=> setFont(true)}/>
+		);
+	}
+
 }
 
 const styles = StyleSheet.create({
